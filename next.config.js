9fcb91f976f1f3b5cf4d23f182dd659d34f83577
moatassimesad <saadounmtsm@gmail.com>
2025-07-26 13:25:36 +0100

    First commit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
